48a112f91c864352743ca04c0ad5ed731a0f7f7e
Albert Kozak/System Innovation /SEPM/Associate/삼성전자 <abik11@gmail.com>
2018-10-01 08:47:35 +0200

    improved merge-nlogs

--- Dev/Merge-NLogs.psm1
+++ Dev/Merge-NLogs.psm1
@@ -5,13 +5,15 @@
     This function merges all the logs from txt log files gathered in one
     directory. It is supposed to work with NLog, but if the logs are in the
     right format, it doesn't really matter how they are generated. The default
-    separator is |, and the format of the logs should be like this:
+    separators are | and space, and the format of the logs can be one of these:
 
-        Name|Date|Log Type|Caller Function/Method|Message
+        Name|Date|Log_Type|Caller_Function/Method|Machine|Message
+        Name Date Log_Type Caller_Function/Method Machine Message
 
     Which in NLog.config can correspond to log layout like this:
 
-        APP LOGIC Module|${longdate}|${level}|${callsite}|${message}
+        APP_Logic_Module|${longdate}|${level}|${callsite}|${machinename}|${message}
+        APP_Logic_Module ${longdate} ${level} ${callsite} ${machinename} ${message}
 
     It is a really nice idea to pipe the result of this function to Out-GridView
     cmdlet, being able to visualy analyze the logs, filter and sortem them.
@@ -28,19 +30,23 @@ function Merge-NLogs {
     [cmdletbinding()]
     Param(
         $directory,
-        [string] $delimeter = '\|'
+        [string] $delimeter = '\||\ '
     )
 
     $logs = Get-ChildItem $directory *.txt | ForEach-Object {
-        $file = Get-Content $_
+        $fileName = $_.FullName
+        $file = Get-Content $fileName
+        
         $file | ForEach-Object {
             $log = $_ -split $delimeter
             [PSCustomObject]@{ 
                 Module=$log[0]; 
-                Date=$log[1];
-                Type=$log[2];
-                Caller=$log[3];
-                Other=$log[4];
+                Date=$log[1,2] -join ' ';
+                Type=$log[3];
+                Caller=$log[4];
+                Machine=$log[5];
+                Message=$log[6..($log.Length-1)] -join ' ';
+                File=$fileName
             }
         }
     }
