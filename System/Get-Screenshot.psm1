c6e705fb8531b9757d7b9eb2519559908b695eb5
Albert Kozak/System Innovation /SEPM/Associate/삼성전자 <abik11@gmail.com>
2018-11-19 14:02:47 +0100

    Get-Screenshot + Get-UserDomainGroups

---
+++ System/Get-Screenshot.psm1
@@ -0,0 +1,31 @@
+ <#
+    .SYNOPSIS
+    Get-Screenshot
+    .EXAMPLE
+    Get-Screenshot 1600 900 C:\img.png
+    .NOTES
+     author: Albert
+#>
+function Get-Screenshot {
+    [cmdletbinding()]
+    Param(
+        [int] $width,
+        [int] $height,
+        [string] $path = 'C:\Temp\screenshot.png'
+    )
+
+    [Reflection.Assembly]::LoadWithPartialName("System.Drawing") | Out-Null
+    $bounds = New-Object -TypeName System.Drawing.Rectangle
+    $bounds.Width = $width
+    $bounds.Height = $height
+
+    $bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
+    $graphics = [System.Drawing.Graphics]::FromImage($bmp)
+    $graphics.CopyFromScreen($bounds.Location, [System.Drawing.Point]::Empty, $bounds.size)
+
+    $bmp.Save($path)
+    $graphics.Dispose()
+    $bmp.Dispose()
+}
+
+Export-ModuleMember -Alias * -Function *
