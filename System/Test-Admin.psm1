2e52191cd5c53ce2cf7ee9bb2cfc863763f015f1
abik11 <abik11@gmail.com>
2020-12-26 22:46:12 +0100

    Update Test-Admin.psm1

--- System/Test-Admin.psm1
+++ System/Test-Admin.psm1
@@ -1,4 +1,5 @@
 function Test-Admin {
     $user = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
     return $user.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    #return $host.UI.RawUI.WindowTitle.Contains("Administrator")
 }
